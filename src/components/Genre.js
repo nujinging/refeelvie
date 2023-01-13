eac3a2b6543db3e46d7dd3cf1e46d9fb5bf465b1
iikasam <iikasam@naver.com>
2023-01-13 17:23:30 +0900

    영화장르

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,7 +1,7 @@
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import {movieApi} from '../axios';
+import { movieApi } from '../axios';
 import { useState, useEffect } from 'react';
 
 // Import Swiper styles
@@ -12,30 +12,46 @@ import "swiper/css/navigation";
 
 
 export default function Genre() {
+  const [images] = useState('https://image.tmdb.org/t/p/w342');
+  const [popularList, setPopularList] = useState([]);
   const [movie, setMovie] = useState([]);
   async function genre() {
-      const itemList = await movieApi.genre('movie')
-      const bannerList = itemList.data.genres;
-      setMovie(bannerList)
+    const itemList = await movieApi.genre('movie')
+    const popularList = await movieApi.popular('movie')
+    const popularResults = popularList.data.results
+    const bannerList = itemList.data.genres;
+    setPopularList(popularResults)
+    setMovie(bannerList)
   }
 
-  console.log(movie)
+  console.log(popularList)
 
   useEffect(() => {
-      genre();
+    genre();
   }, []);
 
-    return (
-      <>
+  return (
+    <>
       <div className="item_container">
-      <Swiper slidesPerView={'auto'} className="mySwiper genre_title">
+        <Swiper slidesPerView={'auto'} className="mySwiper genre_title">
           {movie && movie.map(item => (
             <SwiperSlide className="genre_item" key={item.id}>
               <h3> {item.name} </h3>
             </SwiperSlide>
           ))}
         </Swiper>
+        <ul class="ganre_list">
+          {popularList && popularList.map(item => (
+            <li class="list_card">
+              <picture>
+              <img src={`${images}${item.poster_path}`} alt="background_image"/>
+              </picture>
+              <p class="tit">{item.title} </p>
+            </li>
+          ))}
+        </ul>
+
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
